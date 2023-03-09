ee3a82a0a44a790370752b1d3ee608e8a260d7e3
numselli <58607232+numselli@users.noreply.github.com>
2023-03-09 09:34:07 -0800

    add db

---
+++ schema.postgresql.sql
@@ -0,0 +1,45 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+ALTER SCHEMA public OWNER TO disstat;
+
+COMMENT ON SCHEMA public IS 'standard public schema';
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+CREATE TABLE public.botlist (
+    botid character varying NOT NULL,
+    ownerid character varying NOT NULL
+);
+ALTER TABLE public.botlist OWNER TO disstat;
+ALTER TABLE ONLY public.botlist
+    ADD CONSTRAINT botlist_pkey PRIMARY KEY (botid, ownerid);
+
+
+CREATE TABLE public.owners (
+    ownerid character varying NOT NULL,
+    username character varying NOT NULL
+);
+ALTER TABLE public.owners OWNER TO disstat;
+ALTER TABLE ONLY public.owners
+    ADD CONSTRAINT owners_pkey PRIMARY KEY (ownerid);
+
+CREATE TABLE public.bots (
+    botid character varying NOT NULL,
+    username character varying NOT NULL,
+    icon character varying NOT NULL,
+    token character varying NOT NULL
+);
+ALTER TABLE public.bots OWNER TO disstat;
+ALTER TABLE ONLY public.bots
+    ADD CONSTRAINT bots_pkey PRIMARY KEY (botid);
