64dd8064a1f81c34a9eafa46313a55e36c4efec8
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-03-09 15:34:31 +0100

    yeet api for now

--- site/config/config.mjs
+++
@@ -1,8 +0,0 @@
-export const cookieSecret = 'sdafjksa,dsakd.fbnksladfjnslakdfjsalkdfjasl;fds'
-
-export const oauth2 = {
-    "clientID": "1080913163077308436",
-    "clientSecret": "Ot-b_PAYeI",
-    "redirectUri": "https://disstat.pages.dev/me",
-    "apihost": "https://local.numselli.xyz/api"
-}
--- site/utils/redis.mjs
+++
@@ -1,3 +0,0 @@
-import Redis from "ioredis"
-
-export default new Redis(6379, process.env.NODE_ENV === "production" ? "cache" : "127.0.0.1")
