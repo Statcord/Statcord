6c8c7d99a46a5eb82da5ce11cf412ee3ebc5cad3
numselli <58607232+numselli@users.noreply.github.com>
2023-03-09 15:25:18 -0800

    temporarily remove un-used routes

--- apiSrc/routes/apiRoutes.mjs
+++ apiSrc/routes/apiRoutes.mjs
@@ -1,6 +1,3 @@
-import {route as main} from './root/main.mjs'
-import {route as avatar} from './root/avatar.mjs'
-
 import {route as oauthCallback} from './oauth/callback.mjs'
 import {route as oauthLogIn} from './oauth/login.mjs'
 import {route as ouathLogOut} from './oauth/logout.mjs'
@@ -9,18 +6,8 @@ import {route as oauthUser} from './oauth/user.mjs'
 import {route as botAdd} from './bots/add.mjs'
 import {route as botList} from './bots/list.mjs'
 import {route as mybots } from './bots/mybots.mjs'
-import {route as botGet} from './bots/get.mjs'
-
-import {route as devGetBot} from './devRoutes/get.mjs'
-import {route as devUpdateBot} from './devRoutes/update.mjs'
-import {route as devPostCmd} from './devRoutes/cmd.mjs'
-
-import {route as userGetAPIKey} from './user/getAPIKey.mjs'
-import {route as userRegenAPIKey} from './user/regenAPIKey.mjs'
 
 export default [
-    main,
-    avatar,
     oauthCallback,
     oauthLogIn,
     ouathLogOut,
@@ -28,10 +15,4 @@ export default [
     botAdd,
     botList,
     mybots,
-    botGet,
-    devGetBot,
-    devUpdateBot,
-    devPostCmd,
-    userGetAPIKey,
-    userRegenAPIKey
 ]
--- apiSrc/utils/postgres.mjs
+++ apiSrc/utils/postgres.mjs
@@ -5,17 +5,3 @@ const { pgConfig } = await import(process.env.NODE_ENV === "production" ? '/conf
 const sql = postgres(pgConfig)
 
 export default sql
-
-export async function getUser(id) {
-	return await sql`SELECT * FROM users WHERE id = ${id}`
-}
-export async function updateUser(id, data) {
-	return await sql`UPDATE users SET ${sql(data, ...Object.keys(data))} WHERE id = ${id}`
-}
-
-export async function getBot(id) {
-	return await sql`SELECT * FROM bots WHERE id = ${id}`
-}
-export async function updateBot(id, data) {
-	return await sql`UPDATE bots SET ${sql(data, ...Object.keys(data))} WHERE id = ${id}`
-}
