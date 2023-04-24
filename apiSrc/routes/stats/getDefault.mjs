59d033176836181d4e51442118411164b3c8daa1
numselli <58607232+numselli@users.noreply.github.com>
2023-04-23 19:07:14 -0700

    fix date selector

--- apiSrc/routes/stats/getDefault.mjs
+++ apiSrc/routes/stats/getDefault.mjs
@@ -44,13 +44,13 @@ export const route = {
 	},
 	handler: async (request, reply) => {
 		const statsData = await influx.query(
-			(request.query.star && request.query.end) ? 
-			`SELECT commandsRun, cpuUsage, guildCount, members, ramUsage, shardCount, totalRam, userCount FROM botStats WHERE botid = $botid AND time >= $startDate AND time <= $endDate ORDER BY time ASC` : 
+			(request.query.start && request.query.end) ? 
+			`SELECT commandsRun, cpuUsage, guildCount, members, ramUsage, shardCount, totalRam, userCount FROM botStats WHERE botid = $botid AND time >= $startdate AND time <= $enddate ORDER BY time ASC` : 
 			`SELECT commandsRun, cpuUsage, guildCount, members, ramUsage, shardCount, totalRam, userCount FROM botStats WHERE botid = $botid ORDER BY time ASC`, {
 			placeholders:(request.query.start && request.query.end) ? {
 				botid: request.params.id,
-				startDate: new Date(request.query.start).toISOString(),
-				endDate: new Date(request.query.end).toISOString()
+				startdate: new Date(Number(request.query.start)).toISOString(),
+				enddate: new Date(Number(request.query.end)).toISOString()
 			} : {
 				botid: request.params.id
 			}
