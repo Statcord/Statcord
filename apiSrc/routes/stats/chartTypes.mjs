af1e1241a9c361147583333b96d9989983c45f77
numselli <58607232+numselli@users.noreply.github.com>
2023-05-12 16:33:55 -0700

    remove debug code

--- apiSrc/routes/stats/chartTypes.mjs
+++ apiSrc/routes/stats/chartTypes.mjs
@@ -26,18 +26,6 @@ export const route = {
         }
 	},
 	handler: async (request, reply) => {
-
-		const bots = ['961433265879801936', '901245095502819358', '939114998700048436']
-
-		bots.map(bot=>{
-			Object.keys(defaultChartSettings).map(async key =>{
-				const item = defaultChartSettings[key]
-				db`INSERT INTO chartsettings(botid, chartid, name, label, type) VALUES (${bot}, ${key}, ${item.name}, ${item.label}, ${item.type})`.catch(err=>{})
-			})
-
-		})
-
-
 		const bot = await db`SELECT public, ownerid FROM bots WHERE botid = ${request.params.id}`.catch(err=>{})
         if (!bot[0]) return reply.status(404).send({message: "The bot with the specified ID does not exist!"})
         if (!bot[0].public && bot[0].ownerid !== request.session.discordUserInfo?.id) return reply.status(401).send({message: "You do not have permission to see this bot"})
