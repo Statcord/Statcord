6bb2a6ba091e04a4ce3127618330db18034e1a95
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-05-31 22:04:39 +0200

    register manifest + add serviceworker

---
+++ siteSrc/public/serviceworker.js
@@ -0,0 +1,34 @@
+self.addEventListener("install", event => {
+	event.waitUntil((async () => {
+		const cache = await caches.open("offline")
+		await cache.addAll(["/"])
+	})())
+})
+self.addEventListener("activate", event => {
+	event.waitUntil((async () => {
+		if ("navigationPreload" in self.registration) await self.registration.navigationPreload.enable()
+	})())
+
+	self.clients.claim()
+})
+
+self.addEventListener("fetch", event => {
+	if (event.request.method == "GET" && event.request.mode == "navigate") {
+		event.respondWith((async () => {
+			try {
+				const preloadResponse = await event.preloadResponse
+				if (preloadResponse) return preloadResponse
+
+				const response = await fetch(event.request)
+				cache.put(event.request, response.clone())
+				return response
+			} catch (e) {
+				console.warn("Cannot fetch, serving from cache", e)
+
+				const cache = await caches.open("offline")
+				const cachedResponse = await cache.match(event.request.url)
+				return cachedResponse
+			}
+		})())
+	}
+})
