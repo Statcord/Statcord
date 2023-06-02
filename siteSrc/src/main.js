ca0eb6e4e48a710a312864a05e64a4ad2bc90830
numselli <58607232+numselli@users.noreply.github.com>
2023-06-02 15:27:27 -0700

    fix PWA???

--- siteSrc/src/main.js
+++ siteSrc/src/main.js
@@ -4,6 +4,7 @@ import { createHead } from '@vueuse/head'
 import App from './App.vue'
 
 import router from './router'
+import './registerServiceWorker';
 
 const head = createHead()
 createApp(App).use(head).use(router).mount('#app')
