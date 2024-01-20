1c6ddcede6dfb58a3fd4caba2199d589b16b2fde
numselli <58607232+numselli@users.noreply.github.com>
2024-01-19 19:16:42 -0800

    Update export.mjs

--- siteSrc/server/api/bots/[botID]/stats/export.mjs
+++ siteSrc/server/api/bots/[botID]/stats/export.mjs
@@ -11,32 +11,17 @@ export default defineEventHandler(async event => {
 	const isOwner = !!event.context.session.accessToken && bot[0].ownerid === event.context.session.userInfo.id
 	if ((!bot[0].public && !isOwner)) return sendError(event, createError({statusCode: 401, statusMessage: 'Unauthorized'}))
 
-	const start = 0
-	const stop = new Date().toISOString()
-
-	const mainStats = await fetchFromInflux({
-		measurement: "botStats",
-		start,
-		stop,
-		influxClient: event.context.influx.influxClient,
+	const runInfluxQuery = new influxRun({
+		event,
+		start: 0,
+		stop: new Date().toISOString(),
+		groupBy: "1d",
 		botID: path.botID
 	})
 
-	const commands = await fetchFromInflux({
-		measurement: "customCharts",
-		start,
-		stop,
-		influxClient: event.context.influx.influxClient,
-		botID: path.botID
-	})
-
-	const custom = await fetchFromInflux({
-		measurement: "topCommands",
-		start,
-		stop,
-		influxClient: event.context.influx.influxClient,
-		botID: path.botID
-	})
+	const mainStats = await runInfluxQuery.runQuery("botStats")
+	const commands = await runInfluxQuery.runQuery("topCommands")
+	const custom = await runInfluxQuery.runQuery("customCharts")
 
 	const ownerInfo = await event.context.pgPool`SELECT * FROM owners WHERE ownerid = ${bot[0].ownerid}`.catch(() => {})
 	const chartSettings = await event.context.pgPool`SELECT * FROM chartsettings WHERE botid = ${path.botID}`.catch(() => {})
@@ -45,7 +30,6 @@ export default defineEventHandler(async event => {
 		botInfo: bot[0],
 		ownerInfo: ownerInfo[0],
 		chartSettings: chartSettings,
-		// botSettings,
 		botStats:{
 			mainStats,
 			commands,
@@ -102,31 +86,34 @@ export const schema = {
 	}
 }
 
-const fetchFromInflux = async (options) => {
-	const queryApi = options.influxClient.getQueryApi("disstat")
 
-	const fluxQuery = flux`import "math"
-	from(bucket:"defaultBucket")
-	|> range(start: time(v: ${options.start}), stop: time(v: ${options.stop}))
-	|> filter(fn: (r) => r._measurement == ${options.measurement})
-	|> filter(fn: (r) => r["botid"] == ${options.botID})
-	|> aggregateWindow(every: ${fluxDuration("1m")}, fn: mean, createEmpty: false)
-    |> map(fn: (r) => ({r with _value: math.round(x: r._value)}))
-	|> yield(name: "mean")`
-	// this slows down requests by 9.92%
-	// |> group(columns: ["_time", "_field"])
-
-	let outData = [];
-	for await (const { values, tableMeta } of queryApi.iterateRows(fluxQuery)) {
-		const tableObject = tableMeta.toObject(values)
-
-		const timeIndex = outData.findIndex(element => element.time === tableObject._time)
-		if (timeIndex === -1) outData.push({
-			time: tableObject._time,
-			[tableObject._field]: tableObject._value
-		})
-		else outData[timeIndex][tableObject._field] = tableObject._value
+const influxRun = class{
+	#queryApi
+	#botID
+	#start
+	#end
+	#groupBy
+	constructor(options){
+		this.#queryApi = options.event.context.influx.influxClient.getQueryApi("disstat")
+		this.#botID = options.botID
+		this.#start = options.start
+		this.#end = options.stop
+		this.#groupBy = options.groupBy
 	}
-
-	return outData;
+	async runQuery(measurement){
+		const fluxQuery = flux`from(bucket:"defaultBucket")
+		|> range(start: time(v: ${this.#start}), stop: time(v: ${this.#end}))
+		|> filter(fn: (r) => r._measurement == ${measurement})
+		|> filter(fn: (r) => r["botid"] == ${this.#botID})
+		|> aggregateWindow(every: ${fluxDuration(this.#groupBy ?? "1d")}, fn: mean, createEmpty: false)
+		|> yield(name: "mean")`
+		// |> map(fn: (r) => ({r with _value: math.round(x: r._value)}))
+	
+		let tableObjects = []
+		for await (const { values, tableMeta } of this.#queryApi.iterateRows(fluxQuery)) {
+			tableObjects.push(tableMeta.toObject(values))
+		}
+		
+		return tableObjects
+	}
 }
