b29a7df648fcfc6bbeba271bfa2c15450f26b7ef
numselli <58607232+numselli@users.noreply.github.com>
2024-02-19 17:49:47 -0800

    Update genKey.js

--- siteSrc/server/middleware/genKey.js
+++ siteSrc/server/middleware/genKey.js
@@ -2,7 +2,7 @@ export default defineEventHandler((event) => {
     if (!event.node.req.url || !(event.node.req.url.startsWith('/api/bots/') && event.node.req.url.endsWith('/genKey'))) return;
 
     event.context.genKey = () => {
-        return "DS-xxxxxxxxxxxx4xxxyxxxxxxxx".replace(/[xy]/g, c => {
+        return "SC-xxxxxxxxxxxx4xxxyxxxxxxxx".replace(/[xy]/g, c => {
             const r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8)
             return v.toString(16)
         })
