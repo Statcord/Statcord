b4199711e46fa3d7b53299b8b8102e4b89025840
numselli <58607232+numselli@users.noreply.github.com>
2024-03-02 08:30:47 -0800

    validate bot ownership

--- siteSrc/server/api/bots/add.post.mjs
+++ siteSrc/server/api/bots/add.post.mjs
@@ -74,6 +74,10 @@ export default defineEventHandler(async event => {
 	const botExisits = await event.context.pgPool`SELECT ownerid from bots WHERE botid = ${body.botid}`.catch(() => {})
 	if (botExisits[0]) return sendError(event, createError({statusCode: 409, statusMessage: 'Bot already exists'}))
 
+	const OAuthHelper = event.context.oauth.rest.oauth.getHelper(`Bearer ${event.context.session.accessToken}`)
+    const ownsBot = await OAuthHelper.ownsApplication(body.botid);
+	if (!ownsBot) return sendError(event, createError({statusCode: 401, statusMessage: 'Unauthorized'}))
+
 	const bot = await event.context.oauth.rest.users.get(body.botid).catch(e=>{})
 	if (!bot) return sendError(event, createError({statusCode: 404, statusMessage: 'Bot not found'}))
 
