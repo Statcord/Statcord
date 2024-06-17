ad0cf2f3e8d65e4dae1cd07070b4c83b90c5c2c6
numselli <58607232+numselli@users.noreply.github.com>
2024-06-17 16:50:03 -0700

    Update pricing.vue

--- siteSrc/pages/pricing.vue
+++ siteSrc/pages/pricing.vue
@@ -1,78 +1,68 @@
 <template>
     <UContainer>
-        <div class="row" style="gap:10px;">
-            <div class="col s12 m3 offset-m3">
-                <div class="card">
-                    <div class="card-image">
-                        <img src="/img/icon.png" class="responsive-img">
-                        <span class="card-title black-text">Basic</span>
-                    </div>
-                    <div class="card-content">
-                        <div class="center-align">
+        <div class="grid md:grid-cols-2 sm:grid-cols-1 justify-evenly">
+            <div class="max-w-sm overflow-hidden shadow-md rounded-xl">
+                <nuxt-img class="w-full" alt="" src="/img/icon.png" />
+                <div class="px-4 py-4">
+                    <UContainer>
+                        <span class="font-semibold">Basic</span>
+                        <div class="text-center">
                             <div>
-                                <sub style="vertical-align:top;">USD</sub>
-                                <span style="font-weight:bold;">$0.00</span>
+                                <sub class="align-top">USD</sub>
+                                <span class="font-bold">$0.00</span>
                             </div>
                             <div>
                                 <span>PER MONTH</span>
                             </div>
                         </div>
+
                         <div>
-                            <ul class="browser-default">
+                            <ul class="list-disc">
                                 <li>Basic stats tracking</li>
                                 <li>2 Custom charts</li>
                             </ul>
                         </div>
-                    </div>
-                    <div class="card-action">
-                        <form action="/api/stripe/session/checkout" method="POST">
-                            <input type="hidden" name="lookup_key" value="plus">
-                            <input type="hidden" name="test" value="testdata">
-                            <button id="checkout-and-portal-button" type="submit" class="hide">Checkout</button>
-                        </form>
-                        <div class="waves-effect waves-light btn disabled" style="width: 100%;">
-                            <h6>Join</h6>
-                        </div>
-                    </div>
+                    </UContainer>
                 </div>
             </div>
 
-            <div class="col s12 m3">
-                <div class="card">
-                    <div class="card-image">
-                        <img src="/img/icon.png" class="responsive-img">
-                        <span class="card-title black-text">Advanced</span>
-                    </div>
-                    <div class="card-content">
-                        <div class="center-align">
+            <div class="max-w-sm overflow-hidden shadow-md rounded-xl">
+                <nuxt-img class="w-full" alt="" src="/img/icon.png" />
+                <div class="px-4 py-4">
+                    <UContainer>
+                        <span class="font-semibold">Advanced</span>
+                        <div class="text-center	">
                             <div>
-                                <sub style="vertical-align:top;">USD</sub>
-                                <span style="font-weight:bold;">$ ??</span>
+                                <sub class="align-top">USD</sub>
+                                <span class="font-bold place-content-center">$?.??</span>
                             </div>
                             <div>
                                 <span>PER MONTH</span>
                             </div>
                         </div>
+
+
                         <div>
-                            <ul class="browser-default">
+                            <ul class="list-disc">
                                 <li>Basic stats tracking</li>
                                 <li>More custom charts</li>
                                 <li>Toggle chart visibility</li>
-                                <li>Toggle card visibility<sub style="vertical-align:top;">1</sub></li>
-                                <li>Custom bot url<sub style="vertical-align:top;">2</sub></li>
+                                <li>Toggle card visibility<sub class="align-top">1</sub></li>
+                                <li>Custom bot url<sub class="align-top">2</sub></li>
                             </ul>
                         </div>
-                    </div>
-                    <div class="card-action">
-                        <form action="/api/stripe/session/checkout" method="POST">
-                            <input type="hidden" name="lookup_key" value="plus">
-                            <input type="hidden" name="test" value="testdata">
-                            <button id="checkout-and-portal-button" type="submit" class="hide">Checkout</button>
-                        </form>
-                        <div class="waves-effect waves-light btn disabled" style="width: 100%;">
-                            <h6>Join</h6>
-                        </div>
-                    </div>
+                    </UContainer>
+                </div>
+                <div class="bg-blue-400 text-center text-xl cursor-pointer">
+                    <span class="pt-6">Join</span>
+                    <!-- <form action="/api/stripe/session/checkout" method="POST"> -->
+                        <!-- <input type="hidden" name="lookup_key" value="plus">
+                        <input type="hidden" name="test" value="testdata"> -->
+                        <!-- <button id="checkout-and-portal-button" type="submit" class="">Checkout</button> -->
+                    <!-- </form> -->
+                    <!-- <div class=""> -->
+                        <!-- <h6>Join</h6> -->
+                    <!-- </div> -->
                 </div>
             </div>
         </div>
