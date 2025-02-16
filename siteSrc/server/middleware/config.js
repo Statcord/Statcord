f754a0c52534ff02cc58e2e8601e1630012b1056
numselli <58607232+numselli@users.noreply.github.com>
2025-02-15 22:15:36 -0800

    update middlewares

--- siteSrc/server/middleware/config.js
+++ siteSrc/server/middleware/config.js
@@ -4,6 +4,8 @@ export default defineEventHandler((event) => {
     const url = event.node.req.url.split("?")[0]
 
     switch (url){
+        case "/api/logan/stats/":
+        case "/api/v3/stats/":
         case "/api/logan/stats":
         case "/api/v3/stats":
             event.context.newstwebhook = configFile.webhooks.newSt
