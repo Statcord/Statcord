9352ff7f5600c650d19b614f49b824a29ed05c59
numselli <58607232+numselli@users.noreply.github.com>
2025-10-31 15:32:19 -0700

    Update index.mjs

--- siteSrc/server/api/user/[userID]/index.mjs
+++ siteSrc/server/api/user/[userID]/index.mjs
@@ -4,7 +4,7 @@ export default defineEventHandler(async event => {
     const path = getRouterParams(event)
 
 	const userFromDB = await event.context.pgPool`SELECT plevel, avatar, aboutme, username, website, public FROM owners WHERE ownerid = ${path.userID}`.catch(() => {})
-
+	if (!userFromDB[0]) return sendError(event, createError({statusCode: 404, statusMessage: 'User not found'}))
 	if (!userFromDB[0].public && path.userID !== event.context.session?.userInfo.id) return sendError(event, createError({statusCode: 401, statusMessage: 'Unauthorized'}))
 
 	return {
