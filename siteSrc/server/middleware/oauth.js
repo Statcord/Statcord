d9c88faf8b7fa5ae5408d278f20894694d67211b
numselli <58607232+numselli@users.noreply.github.com>
2025-12-12 13:09:29 -0800

    try something else for oauth

--- siteSrc/server/middleware/oauth.js
+++ siteSrc/server/middleware/oauth.js
@@ -7,27 +7,30 @@ const client = new Client({
     auth: configFile.discord.botToken
 });
 
-const oauth = {
-    ...client,
-    exchangeCode,
-    getHelper: client.rest.oauth.getHelper,
-    getUser: client.rest.users.get
-}
-
-async function exchangeCode(options){
-    const tokens = await client.rest.oauth.exchangeCode({
-        code: options.code,
-        clientSecret: configFile.discord.clientSecret,
-        clientID: configFile.discord.botID,
-        redirectURI: configFile.domain + "/api/oauth/callback"
-    }).catch(e => console.log(e));
-
-    return {
-        redirect: configFile.domain,
-        tokens
-    }
-}
+const allowedRotes = [
+    "/api/oauth/callback",
+    '/api/bots/add',
+    "/settings/sync"
+]
 
 export default defineEventHandler((event) => {
-    event.context.oauth = oauth
+    console.log(event.path)
+    if (!allowedRotes.some(a=>event.path.includes(a))) return;
+
+    event.context.oauth = {
+        ...client,
+        async exchangeCode(options){
+            const tokens = await client.rest.oauth.exchangeCode({
+                code: options.code,
+                clientSecret: configFile.discord.clientSecret,
+                clientID: configFile.discord.botID,
+                redirectURI: configFile.domain + "/api/oauth/callback"
+            }).catch(e => console.log(e));
+
+            return {
+                redirect: configFile.domain,
+                tokens
+            }
+        }
+    }
 })
