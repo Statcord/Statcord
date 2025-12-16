ad1c62a339d8f37a325644c6bb98955b284e147b
numselli <58607232+numselli@users.noreply.github.com>
2025-12-16 08:52:53 -0800

    Update index.mjs

--- siteSrc/server/api/user/[userID]/index.mjs
+++ siteSrc/server/api/user/[userID]/index.mjs
@@ -3,8 +3,9 @@ import { defineEventHandler, createError, getRouterParams, sendError } from "h3"
 export default defineEventHandler(async event => {
     const path = getRouterParams(event)
 
-	const userFromDB = await event.context.pgPool`SELECT plevel, avatar, aboutme, username, website, public FROM owners WHERE ownerid = ${path.userID}`.catch(() => {})
+	const userFromDB = await event.context.pgPool`SELECT avatar, aboutme, username, website, public FROM owners WHERE ownerid = ${path.userID}`.catch(() => {})
 	if (!userFromDB[0]) return sendError(event, createError({statusCode: 404, statusMessage: 'User not found'}))
+	
 	const session = await event.context.session(event);
 	if (!userFromDB[0].public && path.userID !== session?.userInfo.id) return sendError(event, createError({statusCode: 401, statusMessage: 'Unauthorized'}))
 
