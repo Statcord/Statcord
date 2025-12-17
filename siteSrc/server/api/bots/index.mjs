4a544834a68da3043a35a29ec3fb3b802a909f79
numselli <58607232+numselli@users.noreply.github.com>
2025-12-17 02:16:44 -0800

    update bot page

--- siteSrc/server/api/bots/index.mjs
+++ siteSrc/server/api/bots/index.mjs
@@ -2,14 +2,19 @@ import { defineEventHandler, getQuery } from "h3"
 
 export default defineEventHandler(async event => {
     const bo = await event.context.pgPool`SELECT username, avatar, botid, nsfw, shortdesc, lastact FROM bots WHERE public = true AND flags != 1 and lastact >= NOW() - INTERVAL '3 days' LIMIT 40 OFFSET 40*${Number(getQuery(event).page ?? 0)}`.catch(()=>{})
-    return Promise.all(bo.map(async b=>{
-        const g = await event.context.pgPool`select guildcount from mainstats WHERE botid = ${b.botid} order by timestamp desc limit 1`.catch(()=>{})
-        return {
-            ...b,
-            lat: new Date(b.lastact).toDateString()==new Date().toDateString(), 
-            gl:g[0]?.guildcount
+    
+    const a = await event.context.pgPool.begin(async sql => bo.map(async b => {
+        return sql`select guildcount from mainstats WHERE botid = ${b.botid} order by timestamp desc limit 1`
+	}))
+	
+	return await Promise.all(a.map(async (b, i) => {
+		const waitedB = await b
+		return {
+            ...bo[i],
+            lat: new Date(bo[i].lastact).toDateString()==new Date().toDateString(), 
+            gl:waitedB?.guildcount
         }
-    }))
+	}))
 })
 
 export const schema = {
--- siteSrc/server/api/user/[userID]/bots/index.mjs
+++ siteSrc/server/api/user/[userID]/bots/index.mjs
@@ -3,17 +3,20 @@ import { defineEventHandler, getQuery, getRouterParams } from "h3"
 export default defineEventHandler(async event => {
     const path = getRouterParams(event)
 
-	const bo = await event.context.pgPool`SELECT username, avatar, botid, nsfw, shortdesc FROM bots WHERE ownerid = ${path.userID} LIMIT 30 OFFSET 30*${Number(getQuery(event).page ?? 0)}`.catch().catch(() => {})
-	const botids = bo.map(a=>a.botid)
-	const active = await event.context.pgPool`select distinct botid, max(timestamp) from mainstats where timestamp >= NOW() - INTERVAL '3 days' and botid in ${event.context.pgPool(botids)} group by botid`.catch(() => {})
-	return Promise.all(bo.map(async b=>{
-		const g = await event.context.pgPool`select guildcount from mainstats WHERE botid = ${b.botid} order by timestamp desc limit 1`.catch(()=>{})
-        return {
-            ...b,
-            r: typeof active.find(a=>a.botid==b.botid) == 'undefined',
-            gl:g[0]?.guildcount
+	const bo = await event.context.pgPool`SELECT lastact, username, avatar, botid, nsfw, shortdesc FROM bots WHERE ownerid = ${path.userID} LIMIT 30 OFFSET 30*${Number(getQuery(event).page ?? 0)}`.catch().catch(() => {})
+	
+    const a = await event.context.pgPool.begin(async sql => bo.map(async b => {
+        return sql`select guildcount from mainstats WHERE botid = ${b.botid} order by timestamp desc limit 1`
+	}))
+	
+	return await Promise.all(a.map(async (b, i) => {
+		const waitedB = await b
+		return {
+            ...bo[i],
+            lat: new Date(bo[i].lastact).toDateString()==new Date().toDateString(), 
+            gl:waitedB?.[0]?.guildcount
         }
-    }))
+	}))
 })
 
 export const schema = {
